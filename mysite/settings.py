f0bebbdcf6256569f55999df52abf6a37662a5ec
mucharomtzaka <mucharomtzaka@yahoo.co.id>
2021-07-12 13:52:05 +0700

    Update settings.py
    Clean key for commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2&g@139egsl(m@d*d04@myv=y4pnu&an)e(+9a6n08uu20q@%7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
